2d89a1f1f72420b68062e2a6db7093c8bfb00541
Roman <romanslex@gmail.com>
2019-03-04 10:22:18 +0300

    Создал Nav компонент

---
+++ src/containers/Nav.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import {connect} from "react-redux";
+
+class Nav extends Component {
+    loginBtn() {
+        if (this.props.user.isLoggedIn)
+            return (
+                <li className="nav-item"><span style={{cursor: 'pointer'}} className='nav-link'>Выйти</span></li>
+            );
+        return <li className="nav-item"><Link className='nav-link' to='/login'>Войти</Link></li>;
+    }
+
+    render() {
+        return (
+            <nav className="navbar navbar-expand-md navbar-light">
+                <div className="container">
+                    <div id="navbarSupportedContent" className="collapse navbar-collapse">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item"><Link className='nav-link' to='/main'>Главная</Link></li>
+                            <li className="nav-item"><Link className='nav-link' to='/news'>Новости</Link></li>
+                            {this.loginBtn()}
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        user: state.user
+    }
+};
+
+export default connect(mapStateToProps)(Nav)
