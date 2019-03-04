8738735ce69bde6d57233d8b4d4a28b431c729b5
Roman <romanslex@gmail.com>
2019-03-04 10:39:20 +0300

    Добавил в App отображение компонентов по маршруту

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 import React, {Component} from 'react';
+import {Route} from 'react-router-dom';
 import Nav from '../containers/Nav';
+import Main from '../containers/Main';
 
 class App extends Component {
     render() {
         return (
             <div>
                 <Nav/>
+                <main className="py-4">
+                    <div className="container">
+                        <div className="row justify-content-center">
+                            <div className="col-md-8">
+                                <Route path='/main' component={Main}/>
+                            </div>
+                        </div>
+                    </div>
+                </main>
             </div>
         );
     }
