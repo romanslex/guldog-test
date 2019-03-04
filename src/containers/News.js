d5394523db87319eb8f225539354e072fd9d824d
Roman <romanslex@gmail.com>
2019-03-04 12:06:58 +0300

    Страница новостей

---
+++ src/containers/News.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import {connect} from "react-redux";
+
+class News extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            search: ''
+        };
+        this.searchChange = this.searchChange.bind(this);
+    }
+
+    searchChange(e) {
+        this.setState({search: e.target.value});
+    }
+
+    render() {
+        let news = this.props.news(this.state.search);
+        return (
+            <div>
+                <input value={this.state.search} onChange={this.searchChange} type="text"/>
+                {
+                    news.map(i => {
+                        return (
+                            <div className="media" key={i.id} style={{marginTop: '20px'}}>
+                                <div className="media-body">
+                                    <h5 className="mt-0">{i.header}</h5>
+                                    {i.text}
+                                </div>
+                            </div>
+                        );
+                    })
+                }
+            </div>
+
+        );
+    }
+}
+
+
+const mapStateToProps = state => ({
+    news: search => state.news.filter(i =>
+        i.header.toLowerCase().startsWith(search.toLowerCase()) ||
+        i.text.toLowerCase().startsWith(search.toLowerCase())
+    )
+});
+
+export default connect(mapStateToProps)(News)
