ae8456e762a60ec8399cfe438510c9b52b08f350
Roman <romanslex@gmail.com>
2019-03-04 09:57:14 +0300

    Добавил reducers/index.js и подключил provider

--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter as Router} from 'react-router-dom'
+import rootReducer from './reducers';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
-    <Router>
-        <App/>
-    </Router>,
+    <Provider store={store}>
+        <Router>
+            <App/>
+        </Router>
+    </Provider>,
     document.getElementById('root')
 );
 
